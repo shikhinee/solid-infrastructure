d4ea8eb9b1eeb10b11ad903fcebbb5904107b6ca
Jargalsaikhan Erdenetsetseg <thejargalsaikhan@gmail.com>
2021-09-06 15:05:47 +0800

    INTEGRATION: changepass updatebank FE BE

--- pages/dashboard/user/bank-info/index.jsx
+++ pages/dashboard/user/bank-info/index.jsx
@@ -1,17 +1,65 @@
 //Next, React (core node_modules) imports must be placed here
-
+import { useState } from "react";
+import axios from "axios";
 //Fetchers must be imported here
 //import useFETCHER from 'tools/useFETCHER'
 
 //Layout must be imported here
 //import LAYOUT from 'layouts/LAYOUT'
+import PrimaryButton from "@/components/PrimaryButton";
+import StyledForm from "@/components/StyledForm";
 import DashboardLayout from "layouts/Dashboard";
 
 //Component must be imported here
 //import COMPONENT from 'components/COMPONENT'
 
 const BankInfoPage = (props) => {
-  return <main>THIS IS THE USER BANK INFO PAGE</main>;
+  const [formData, setFormData] = useState({
+    bankName: "",
+    bankNumber: "",
+  });
+
+  const handleInputFormData = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    axios.put("/api/updatebank/6135b6521b595cb8b38c8a03", formData);
+  };
+
+  return (
+    <main>
+      <StyledForm onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="bankName">bankName</label>
+          <input
+            type="text"
+            id="bankName"
+            name="bankName"
+            placeholder="bankName"
+            onChange={handleInputFormData}
+          />
+        </div>
+        <div>
+          <label htmlFor="bankNumber">bankNumber</label>
+          <input
+            type="text"
+            id="bankNumber"
+            name="bankNumber"
+            placeholder="bankNumber"
+            onChange={handleInputFormData}
+          />
+        </div>
+        <PrimaryButton>Хадгалах</PrimaryButton>
+      </StyledForm>
+    </main>
+  );
 };
 
 BankInfoPage.Layout = DashboardLayout;
--- pages/dashboard/user/change-pass/index.jsx
+++ pages/dashboard/user/change-pass/index.jsx
@@ -3,21 +3,79 @@
 //Fetchers must be imported here
 //import useFETCHER from 'tools/useFETCHER'
 
+import PrimaryButton from "@/components/PrimaryButton";
+import StyledForm from "@/components/StyledForm";
+import axios from "axios";
+
 //Layout must be imported here
 //import LAYOUT from 'layouts/LAYOUT'
 import DashboardLayout from "layouts/Dashboard";
+import { useState } from "react";
 
 //Component must be imported here
 //import COMPONENT from 'components/COMPONENT'
 
 const ChangePassPage = (props) => {
-  return <main>THIS IS THE USER CHANGE PASS PAGE</main>;
+  const [formData, setFormData] = useState({
+    oldPass: "",
+    newPass: "",
+  });
+
+  const handleInputFormData = (e) => {
+    console.log([e.target.name], e.target.value);
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    axios.put("/api/changepassword/6135b6521b595cb8b38c8a03", formData);
+  };
+
+  return (
+    <main>
+      <StyledForm onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="newPass">Шинэ Нууц үг</label>
+          <input
+            type="password"
+            name="newPass"
+            id="newPass"
+            placeholder="Нууц үг"
+            onChange={handleInputFormData}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="oldPass">Одоогийн нууц үг</label>
+          <input
+            type="password"
+            name="oldPass"
+            id="oldPass"
+            placeholder="Нууц үг"
+            onChange={handleInputFormData}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="passwordconfirm">Нууц үг давтах</label>
+          <input
+            type="password"
+            name="passwordconfirm"
+            id="passwordconfirm"
+            placeholder="Нууц үг"
+          />
+        </div>
+        <PrimaryButton>Хадгалах</PrimaryButton>
+      </StyledForm>
+    </main>
+  );
 };
 
 ChangePassPage.Layout = DashboardLayout;
-
-// ChangePassPage.getLayout = (page) => {
-//   return <DashboardLayout>{page}</DashboardLayout>;
-// };
 
 export default ChangePassPage;
